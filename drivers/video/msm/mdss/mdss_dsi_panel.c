fc6957d1e5b9a911d0bc9dfc2994d98aae0653fc
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-28 04:13:24 +0000

    display: fix merge derp -_-
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -43,9 +43,9 @@ static char rc_range_bpg_offset[] = {2, 0, 0, -2, -4, -6, -8, -8, -8, -10, -10,
 
 bool display_on = true;
 
-bool is_display_on()
+bool is_display_on(void)
 {
-	return display_on;
+        return display_on;
 }
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
